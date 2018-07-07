ab26901fd3da5de63474ec55dc1583c2e7babe22
Hiago Silva Souza <hiasilva@gmail.com>
2018-07-07 11:24:11 -0300

    Criado API para testes.

--- ContatoWeb/Controllers/ContatoController.cs
+++ ContatoWeb/Controllers/ContatoController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -9,9 +10,33 @@ namespace ContatoWeb.Controllers
 {
     public class ContatoController : ApiController
     {
+        private SqlConnection getConn()
+        {
+            return new SqlConnection("Server=tcp:hiago-pos.database.windows.net,1433;Initial Catalog=bdcontato;Persist Security Info=False;User ID={your_username};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
         public string Get()
         {
-            return "Olá Mundo, integração contínua tá ok ;)";
+            SqlConnection con = getConn();
+
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.Connection = con;
+            cmd.CommandText = @"SELECT * FROM contato FOR JSON auto";
+
+            SqlDataReader oi = cmd.ExecuteReader();
+
+            con.Close();
+
+            return oi.GetString(0);
         }
+    }
+
+    public class Contato
+    {
+        public int ContatoId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
     }
 }
